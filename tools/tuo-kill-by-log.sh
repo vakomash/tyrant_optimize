df95f8c752b2dec774e600edf3dac16b46c851e4
Evgeniy Kozlov <dsuchka@gmail.com>
2016-04-04 16:36:14 +0300

    tools: add tuo-kill-by-log.sh
      * the helper script for managing simulation processes by logs:
        $ tuo-kill-by-log.sh STOP tuo*.log # pause
        $ tuo-kill-by-log.sh CONT tuo*high-prior-task*.log # unpause

---
+++ tools/tuo-kill-by-log.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sign=${1:-0}
+shift
+
+check_tuo_pid() {
+    local -i pid=$1
+    local cmdline="/proc/$pid/cmdline"
+    [[ -r "$cmdline" ]] || return 1
+    cmd=$(basename $(tr '\0' '\n' < "$cmdline" | head -n1))
+    [[ $cmd == tuo ]] || return 2
+    return 0
+}
+
+for i in "$@"; do
+    xname=$(basename "$i")
+    pid=$(head -n1 "$i" | grep -P -o '(?<=^pid: )\d+')
+    if ! check_tuo_pid "$pid"; then
+        echo "$xname => $pid (dead)" 1>&2
+        continue
+    fi
+    echo "$xname => $pid (kill -${sign})" 1>&2
+    kill -"${sign}" "$pid"
+done
