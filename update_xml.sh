8d83b65b762d08a5e69dc013903c8c933c3bb7fa
Nicolas Munera <nm338@cornell.edu>
2017-06-06 15:51:26 -0400

    Update update_xml.sh

--- update_xml.sh
+++ update_xml.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-for fn in fusion_recipes_cj2 missions levels skills_set `seq -f cards_section_%g 1 12` ; do
+for fn in fusion_recipes_cj2 missions levels skills_set `seq -f cards_section_%g 1 13` ; do
   curl http://mobile$1.tyrantonline.com/assets/${fn}.xml -R -z data/${fn}.xml -o data/${fn}.xml
 done
