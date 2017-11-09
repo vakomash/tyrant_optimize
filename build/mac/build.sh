28b6fbf47a3db58d2d19abb122b4fe47ac452b29
APN-Pucky <APN-Pucky@users.noreply.github.com>
2017-11-09 23:00:26 +0100

    Mac Travis Deploy Code

--- build/mac/build.sh
+++ build/mac/build.sh
@@ -4,4 +4,10 @@ set -xue
 
 make rebuild -f Makefile.osx VERSION=$TRAVIS_TAG;
 
+./tuo.osx -version
+
+cp tuo.osx $TUO_FILE_32
+
+cp tuo.osx $TUO_FILE_64
+
 set +xue
--- build/mac/package.sh
+++ build/mac/package.sh
@@ -6,7 +6,12 @@ set -xue
 #DEPLOY_FILE=tuo_@VERSION@_${TUO_PLATFORM}.exe
 
 #cp build-dir/tuo $TUO_FILE
-./tuo.osx -version
+
+export TUO_FILE_ZIP="tuo.${TRAVIS_TAG}_32+64_bit_osx.zip" 
+
+#ls -la
+
+zip -r "$TUO_FILE_ZIP" data/ "$TUO_FILE_32" "$TUO_FILE_64" 
 
 
 set +xue
