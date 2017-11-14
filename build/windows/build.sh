53f9794024b54ce2a0e3fdc4f3732ad5c1b9cf52
APN-Pucky <APN-Pucky@users.noreply.github.com>
2017-11-14 22:38:07 +0100

    Fix Travis all versions x32

--- build/windows/build.sh
+++ build/windows/build.sh
@@ -6,16 +6,16 @@ MXE_DIR=/usr/lib/mxe
 
 MXE_TARGET=i686-w64-mingw32.static
 
-${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake . -Bbuild-dir -DVERSION:STRING="${TRAVIS_TAG}"
-cmake --build build-dir 
+${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake . -Bbuild-dir32 -DVERSION:STRING="${TRAVIS_TAG}"
+cmake --build build-dir32 
 
-mv build-dir/tuo.exe $TUO_FILE_32
+mv build-dir32/tuo.exe $TUO_FILE_32
 
 MXE_TARGET=x86_64-w64-mingw32.static
 
-${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake . -Bbuild-dir -DVERSION:STRING="${TRAVIS_TAG}"
-cmake --build build-dir 
+${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake . -Bbuild-dir64 -DVERSION:STRING="${TRAVIS_TAG}"
+cmake --build build-dir64 
 
-mv build-dir/tuo.exe $TUO_FILE_64
+mv build-dir64/tuo.exe $TUO_FILE_64
 
 set +xue
