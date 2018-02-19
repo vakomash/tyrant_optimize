69a7a88e0f98491c976e2f91f6bfa557141ffab4
APN-Pucky <APN-Pucky@users.noreply.github.com>
2018-02-19 13:03:02 +0100

    2nd Travis Test

--- build/windows/build.sh
+++ build/windows/build.sh
@@ -8,11 +8,12 @@ MXE_TARGET=i686-w64-mingw32.static
 
 ${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake . -Bbuild-dir32-debug -DVERSION:STRING="${TRAVIS_TAG}--debug" -DDEBUG:STRING=""
 cmake --build build-dir32-debug 
-mv build-dir32-debug/tuo.exe $TUO_FILE_32_DEBUG
+#mv build-dir32-debug/tuo.exe $TUO_FILE_32_DEBUG
+mv build-dir32-debug/tuo.exe $TUO_FILE_32
 
-${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake . -Bbuild-dir32 -DVERSION:STRING="${TRAVIS_TAG}" -DDEBUG:STRING="-DNDEBUG"
-cmake --build build-dir32 
-mv build-dir32/tuo.exe $TUO_FILE_32
+#${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake . -Bbuild-dir32 -DVERSION:STRING="${TRAVIS_TAG}" -DDEBUG:STRING="-DNDEBUG"
+#cmake --build build-dir32 
+#mv build-dir32/tuo.exe $TUO_FILE_32
 
 
 
@@ -20,10 +21,11 @@ MXE_TARGET=x86_64-w64-mingw32.static
 
 ${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake . -Bbuild-dir64-debug -DVERSION:STRING="${TRAVIS_TAG}--debug" -DDEBUG:STRING=""
 cmake --build build-dir64-debug 
-mv build-dir64-debug/tuo.exe $TUO_FILE_64_DEBUG
+#mv build-dir64-debug/tuo.exe $TUO_FILE_64_DEBUG
+mv build-dir64-debug/tuo.exe $TUO_FILE_64
 
-${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake . -Bbuild-dir64 -DVERSION:STRING="${TRAVIS_TAG}" -DDEBUG:STRING="-DNDEBUG"
-cmake --build build-dir64 
-mv build-dir64/tuo.exe $TUO_FILE_64
+#${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake . -Bbuild-dir64 -DVERSION:STRING="${TRAVIS_TAG}" -DDEBUG:STRING="-DNDEBUG"
+#cmake --build build-dir64 
+#mv build-dir64/tuo.exe $TUO_FILE_64
 
 set +xue
