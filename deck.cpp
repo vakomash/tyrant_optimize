c400a10ac188a4d6dfc45a2b4ddfdb67fd97a8ad
APN-Pucky <APN-Pucky@users.noreply.github.com>
2018-03-17 07:08:23 +0100

    Add CQ Fortress filter

--- deck.cpp
+++ deck.cpp
@@ -241,7 +241,7 @@ void Deck::set(const std::vector<unsigned>& ids, const std::map<signed, char> &m
             add_dominion(card, false);
             non_deck_cards_seen++;
         }
-        else if (card->m_category == CardCategory::fortress_defense || card->m_category == CardCategory::fortress_siege)
+        else if (card->m_category == CardCategory::fortress_defense || card->m_category == CardCategory::fortress_siege || card->m_category == CardCategory::fortress_conquest)
         {
             fortress_cards.emplace_back(card);
             non_deck_cards_seen++;
