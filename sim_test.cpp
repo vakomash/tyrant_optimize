7f96469e00c744e9f321520cd2b4a267f515f6bb
APN-Pucky <APN-Pucky@users.noreply.github.com>
2019-01-01 18:10:50 +0100

    Load test cases from files

--- sim_test.cpp
+++ sim_test.cpp
@@ -5,12 +5,20 @@
 #define BOOST_TEST_MODULE sim
 
 #include <boost/test/included/unit_test.hpp>
-#include <boost/test/output_test_stream.hpp>
+#include <boost/test/data/test_case.hpp>
+#include <boost/test/data/monomorphic.hpp>
 #include <iostream>
+#include <fstream>
+#include <iostream>
+#include <exception>
+#include <sstream>
 #include "tyrant.h"
 #include "tyrant_optimize.h"
 #include "sim.h"
+#include "read.h"
+
 using namespace std;
+namespace bdata = boost::unit_test::data;
 typedef std::pair<FinalResults<long double>,std::string> Result;
 
 //pipe output: https://stackoverflow.com/questions/5405016/can-i-check-my-programs-output-with-boost-test
@@ -26,6 +34,14 @@ struct cout_redirect {
 private:
     std::streambuf * old;
 };
+
+struct TestInfo{
+  std::string your_deck,enemy_deck,bge;
+};
+std::ostream& operator<<(std::ostream& os, const TestInfo& ti)
+  {
+    return os << "Your Deck: " <<  ti.your_deck << "; Enemy Deck: " << ti.enemy_deck << "; BGE: " << ti.bge;
+  }
 
 inline Result run_sim(int argc,const char** argv)
 {
@@ -58,14 +74,44 @@ inline void check_win(Result result) {
       ,result.second);
     //BOOST_CHECK(100==result.points);
 }
-inline void check_win_sim(const char* your_deck, const char* enemy_deck, const char* bge="") {
+//inline void check_win_sim(const char* your_deck, const char* enemy_deck, const char* bge="") {
+inline void check_win_sim(TestInfo ti) {
     /////////////
     // Max. Iter == 100, else check_win fails with integer vs double equal in check_win
     ////////////
-    const char* argv[] = {"tuo",your_deck,enemy_deck,"-e",bge,"sim", "100"}; //TODO hardcoded iterations? //much output on error?!
+    const char* argv[] = {"tuo",ti.your_deck.c_str(),ti.enemy_deck.c_str(),"-e",ti.bge.c_str(),"sim", "100"}; //TODO hardcoded iterations? //much output on error?!
     Result result(run_sim(sizeof(argv)/sizeof(*argv),argv));
+    //result.second += "\nTest: " + ti.your_deck + "; " + ti.enemy_deck + "; " + ti.bge;
     check_win(result);
 }
+
+
+std::vector<TestInfo> read_test_file(const std::string filename) {
+    std::vector<TestInfo> ret;
+    std::ifstream test_file(filename);
+    if(test_file.good())
+    {
+        try{
+            while(test_file && !test_file.eof())
+            {
+                TestInfo ti;
+                std::string line,yd,ed,bg;
+                std::getline(test_file,line);
+                if(is_line_empty_or_commented(line)){ continue;}
+                std::istringstream iss(line);
+                std::getline(iss,ti.your_deck,';');
+                std::getline(iss,ti.enemy_deck,';');
+                std::getline(iss,ti.bge,';');
+                ret.push_back(ti);
+            }
+        }
+        catch (std::exception& e)
+        {
+        }
+    }
+    return ret;
+}
+
 BOOST_AUTO_TEST_CASE(test_sim_init)
 {
     init();
@@ -77,6 +123,34 @@ BOOST_AUTO_TEST_CASE(test_sim_init)
 /////////////////////////////////////
 // Test Cases !should! be very close fights for maximum sensitivity of errors
 /////////////////////////////////////
+BOOST_AUTO_TEST_SUITE(test_single_units)
+BOOST_DATA_TEST_CASE(test_single_units,bdata::make(read_test_file("test/test_sinlge_units.csv")),ti)
+{
+   check_win_sim(ti);
+}
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(test_multi_units)
+BOOST_DATA_TEST_CASE(test_multi_units,bdata::make(read_test_file("test/test_multi_units.csv")),ti)
+{
+   check_win_sim(ti);
+}
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(test_bges)
+BOOST_DATA_TEST_CASE(test_bges,bdata::make(read_test_file("test/test_bges.csv")),ti)
+{
+   check_win_sim(ti);
+}
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(test_whole_decks)
+BOOST_DATA_TEST_CASE(test_whole_decks,bdata::make(read_test_file("test/test_whole_decks.csv")),ti)
+{
+   check_win_sim(ti);
+}
+BOOST_AUTO_TEST_SUITE_END()
+/*
 BOOST_AUTO_TEST_SUITE(test_single_units)
 //single units to avoid randomness
 BOOST_AUTO_TEST_CASE(test_obsidian_infantry)
@@ -165,5 +239,6 @@ BOOST_AUTO_TEST_CASE(test_m142_m1)
 }
 BOOST_AUTO_TEST_SUITE_END()
 //------------------------
+*/
 #endif
 #endif
