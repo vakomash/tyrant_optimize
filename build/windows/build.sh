2b02e381e5cc50d87cbac8bdb30f21387f88e063
APN-Pucky <APN-Pucky@users.noreply.github.com>
2019-03-21 19:24:31 +0100

    Test OpenMP with MXE

--- build/windows/build.sh
+++ build/windows/build.sh
@@ -9,12 +9,12 @@ MXE_DIR=/usr/lib/mxe
 #Windows x86 Build
 MXE_TARGET=i686-w64-mingw32.static
 
-${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake . -Bbuild-dir32-debug -DVERSION:STRING="${TRAVIS_TAG}--debug"
+${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake . -Bbuild-dir32-debug -DVERSION:STRING="${TRAVIS_TAG}--debug"  -DUSE_OPENMP=ON
 sed -i -e 's/-DNDEBUG//g' build-dir32-debug/CMakeCache.txt #cmake sets dndebug by default
 cmake --build build-dir32-debug
 mv build-dir32-debug/tuo.exe $TUO_FILE_32_DEBUG
 
-${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake . -Bbuild-dir32 -DVERSION:STRING="${TRAVIS_TAG}" -DDEBUG:STRING="-DNTIMER"
+${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake . -Bbuild-dir32 -DVERSION:STRING="${TRAVIS_TAG}" -DDEBUG:STRING="-DNTIMER"  -DUSE_OPENMP=ON
 cmake --build build-dir32
 mv build-dir32/tuo.exe $TUO_FILE_32
 
@@ -22,12 +22,12 @@ mv build-dir32/tuo.exe $TUO_FILE_32
 #Windows x64 Build
 MXE_TARGET=x86_64-w64-mingw32.static
 
-${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake . -Bbuild-dir64-debug -DVERSION:STRING="${TRAVIS_TAG}--debug"
+${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake . -Bbuild-dir64-debug -DVERSION:STRING="${TRAVIS_TAG}--debug"  -DUSE_OPENMP=ON
 sed -i -e 's/-DNDEBUG//g' build-dir64-debug/CMakeCache.txt #cmake sets dndebug by default
 cmake --build build-dir64-debug
 mv build-dir64-debug/tuo.exe $TUO_FILE_64_DEBUG
 
-${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake . -Bbuild-dir64 -DVERSION:STRING="${TRAVIS_TAG}" -DDEBUG:STRING="-DNTIMER"
+${MXE_DIR}/usr/bin/${MXE_TARGET}-cmake . -Bbuild-dir64 -DVERSION:STRING="${TRAVIS_TAG}" -DDEBUG:STRING="-DNTIMER"  -DUSE_OPENMP=ON
 cmake --build build-dir64
 mv build-dir64/tuo.exe $TUO_FILE_64
 
