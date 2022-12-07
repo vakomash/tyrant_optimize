27c90c8679dcd705cb2cd851c64b3693031fee72
Alexander Puck Neuwirth <APN-Pucky@users.noreply.github.com>
2022-12-07 21:09:05 +0100

    Add test workflow (#30)
    * Add test workflow
    
    * Fix yaml run#
    
    * Add checkout to ci
    
    * get xmls
    
    * Add test
    
    * 11->14
    
    * Test ubuntu on more recent
    
    * Data xml url?

--- update_xml.sh
+++ update_xml.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for fn in fusion_recipes_cj2 missions levels skills_set `seq -f cards_section_%g 1 19` ; do
-  curl http://mobile$1.tyrantonline.com/assets/${fn}.xml -R -z data/${fn}.xml -o data/${fn}.xml
+  curl https://mobile$1.tyrantonline.com/assets/${fn}.xml -R -z data/${fn}.xml -o data/${fn}.xml
 done
